68760f0dcf8bdc7c8d7c37c6a0d8a7254fd042b0
DevGaikwad <141038667+DevGaikwad@users.noreply.github.com>
2023-12-09 14:42:39 +0530

    Add files via upload

---
+++ if_else.c
@@ -0,0 +1,35 @@
+// code no:1
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter age :");
+    scanf("%d",&age);
+    printf("enter age is: %d\n",age);
+
+    if(age >=18)
+    {
+        printf("You are eligibal ");
+    }
+    else
+    {
+        printf("You are not eligibal");
+    }
+
+    return 0;
+}
+
+// code no:2
+// #include <stdio.h>
+
+// int main() {
+//   int time = 22;
+//   if (time < 10) {
+//     printf("Good morning.");
+//   } else if (time < 20) {
+//     printf("Good day.");
+//   } else {
+//     printf("Good evening.");
+//   }
+//   return 0;
+// }
